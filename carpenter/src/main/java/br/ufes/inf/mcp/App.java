c0f2da75b84eba19fc2d014ee8c2f5a0c49cb708
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-05-28 16:37:04 -0300

    passo 8 da implementação

--- carpenter/src/main/java/br/ufes/inf/mcp/App.java
+++ carpenter/src/main/java/br/ufes/inf/mcp/App.java
@@ -1,13 +1,35 @@
 package br.ufes.inf.mcp;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.LinkedList;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        Reader in = new FileReader("female-and-male-life-expectancy-at-birth-in-years.csv");
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+        
+        int line = 0;
+        LinkedList <Country>countries = new LinkedList<>();
+
+        for(CSVRecord record : records) {
+
+            if(line == 72){
+                String name = record.get(0);
+                String code = record.get(1);
+                String year = record.get(2);
+                String female_exp = record.get(4);
+                Country c = new Country(name, code, year, female_exp);
+                System.out.println(name + year);
+                countries.add(c);
+                line = 1;
+            }
+            line++;
+        }
     }
 }
---
+++ carpenter/src/main/java/br/ufes/inf/mcp/Country.java
@@ -0,0 +1,12 @@
+package br.ufes.inf.mcp;
+
+public class Country {
+    public String name, code, year, female_exp;
+
+    public Country(String name, String code, String year, String female_exp){
+        this.name = name;
+        this.code = code;
+        this.year = year;
+        this.female_exp = female_exp;
+    }
+}
