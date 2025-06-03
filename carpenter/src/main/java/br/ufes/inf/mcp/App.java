3e8d013b7b13ed580e624b65d001b3bdaca91e9f
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-06-02 21:56:56 -0300

    criação classe country

--- carpenter/src/main/java/br/ufes/inf/mcp/App.java
+++ carpenter/src/main/java/br/ufes/inf/mcp/App.java
@@ -1,13 +1,33 @@
 package br.ufes.inf.mcp;
 
-/**
- * Hello world!
- *
- */
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.LinkedList;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        Reader in = new FileReader("female-and-male-life-expectancy-at-birth-in-years.csv");
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+
+        LinkedList <Country> countries = new LinkedList<>();
+        int line_num = 0;
+        for (CSVRecord record : records) {
+            line_num++;
+            if((line_num - 2) % 72 == 0){
+                String name = record.get(0);
+                String code = record.get(1);
+                String year = record.get(2);
+                String male_exp = record.get(3);
+                String fem_exp = record.get(4);
+                Country c = new Country(name, code, year, male_exp, fem_exp);
+                countries.add(c);
+            }
+        }
     }
 }
---
+++ carpenter/src/main/java/br/ufes/inf/mcp/Country.java
@@ -0,0 +1,9 @@
+package br.ufes.inf.mcp;
+
+public class Country {
+
+    public Country(String name, String code, String year, String male_exp, String fem_exp) {
+        //TODO Auto-generated constructor stub
+    }
+
+}
