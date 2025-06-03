8aef866a7d3ce484c3afae7760af14e8b53eb15f
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-06-02 23:39:04 -0300

    merge ajustado

--- carpenter/src/main/java/br/ufes/inf/mcp/Country.java
+++ carpenter/src/main/java/br/ufes/inf/mcp/Country.java
@@ -1,20 +1,14 @@
 package br.ufes.inf.mcp;
 
 public class Country {
-<<<<<<< HEAD
+    public String name, code, year, male_exp, fem_exp;
 
-    public Country(String name, String code, String year, String male_exp, String fem_exp) {
-        //TODO Auto-generated constructor stub
-    }
-
-=======
-    public String name, code, year, female_exp;
-
-    public Country(String name, String code, String year, String female_exp){
+    public Country(String name, String code, String year, String male_exp, String fem_exp){
         this.name = name;
         this.code = code;
         this.year = year;
-        this.female_exp = female_exp;
+        this.fem_exp = fem_exp;
+        this.male_exp = male_exp;
     }
->>>>>>> c0f2da75b84eba19fc2d014ee8c2f5a0c49cb708
+
 }
